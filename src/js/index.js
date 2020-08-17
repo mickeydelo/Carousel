055a63ec13fb5b1f1c43363f3b16bd4c741333b2
Mickey DeLorenzo <mickeyd@gmail.com>
2020-08-17 10:29:17 -0400

    Initial commit

---
+++ src/js/index.js
@@ -0,0 +1,26 @@
+import EmblaCarousel from "embla-carousel";
+import { setupPrevNextBtns, disablePrevNextBtns } from "./prevAndNextButtons";
+import { setupDotBtns, generateDotBtns, selectDotBtn } from "./dotButtons";
+import "../css/base.css";
+import "../css/reset.css";
+import "../css/embla.css";
+import "../css/header.css";
+import "../css/footer.css";
+
+const wrap = document.querySelector(".embla");
+const viewPort = wrap.querySelector(".embla__viewport");
+const prevBtn = wrap.querySelector(".embla__button--prev");
+const nextBtn = wrap.querySelector(".embla__button--next");
+const dots = document.querySelector(".embla__dots");
+const embla = EmblaCarousel(viewPort, { loop: false });
+const dotsArray = generateDotBtns(dots, embla);
+const setSelectedDotBtn = selectDotBtn(dotsArray, embla);
+const disablePrevAndNextBtns = disablePrevNextBtns(prevBtn, nextBtn, embla);
+
+setupPrevNextBtns(prevBtn, nextBtn, embla);
+setupDotBtns(dotsArray, embla);
+
+embla.on("select", setSelectedDotBtn);
+embla.on("select", disablePrevAndNextBtns);
+embla.on("init", setSelectedDotBtn);
+embla.on("init", disablePrevAndNextBtns);
